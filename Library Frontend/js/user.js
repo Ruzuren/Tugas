59eb2aafcf05c052d6430da94502e10c934f98ad
M Fadhil Al Rasyid <fad.alrasyid@gmail.com>
2021-05-05 11:06:39 +0700

    initial commit
    initial commit

---
+++ Library Frontend/js/user.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {  
+  
+    getUsers().then(users => {
+        const userTable = document.querySelector('#userTable');
+        const tbody = userTable.querySelector('tbody');
+    
+        tbody.innerHTML = '';
+        let sortedUsers = users.sort((a,b) => a.user_id - b.user_id)
+        sortedUsers.forEach(item => {
+            const row = createHTMLRow({
+                id: item.user_id, 
+                name: item.full_name, 
+                username: item.username, 
+                password: item.password, 
+                email: item.email 
+            });
+            tbody.appendChild(row);
+        })    
+    });
+});
